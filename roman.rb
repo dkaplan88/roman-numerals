1a2ea1287b0a28e843cf5907f293b7c7976d5ccd
dkaplan <dkaplan@enova.com>
2014-07-06 19:29:44 -0500

    reducing num < 5 call to just happen once

--- roman.rb
+++ roman.rb
@@ -4,37 +4,26 @@ class Fixnum
                 50 => 'L' , 90 => 'XC' ,100 => 'C' ,
                 400 => 'CD' ,500 => 'D' ,900 => 'CM' ,
                 1000 => 'M' }
+  
+  TENS = { 0 => 'I', 1 => 'X', 2 => 'C', 3 => 'M'}
 
   def to_roman
     to_roman = ''
     
     self.to_s.reverse.chars.each_with_index do |num, index|
       num = num.to_i
-
       numberals = 10 ** index * num
+
       if NUMBERAlS[numberals]
         to_roman = NUMBERAlS[numberals] + to_roman
+      elsif num < 5
+        to_roman = TENS[index] * num + to_roman
       elsif index === 0
-        if num > 5
-          leftover = num - 5
-          to_roman += 'V' + ('I' * leftover)
-        else
-          to_roman += 'I' * num
-        end
+        to_roman += 'V' + ('I' * (num- 5))
       elsif index == 1
-        if num < 5
-          to_roman = ('X' * num) + to_roman
-        else
-          to_roman = 'L' + ('X' * (num - 5)) + to_roman
-        end
+        to_roman = 'L' + ('X' * (num - 5)) + to_roman
       elsif index == 2
-        if num < 5
-          to_roman = ('C' * num) + to_roman
-        else
-          to_roman = 'D' + ('C' * (num - 5)) + to_roman
-        end
-      elsif index == 3
-        to_roman = ('M' * num) + to_roman
+        to_roman = 'D' + ('C' * (num - 5)) + to_roman
       end
     end
 
